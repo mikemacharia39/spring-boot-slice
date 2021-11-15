f1587e87ce2612aba425935c2e7295dcde5b26b0
Mikehenry Maina <mikemacharia39@gmail.com>
2021-11-15 16:00:55 +0300

    set up project start

---
+++ src/main/java/com/mikehenry/springbootslice/SpringBootSliceApplication.java
@@ -0,0 +1,13 @@
+package com.mikehenry.springbootslice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSliceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikehenry/springbootslice/SpringBootSliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikehenry.springbootslice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
